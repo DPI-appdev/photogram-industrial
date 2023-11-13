43a81b19b8924405e2cd750d6cb2df4d5f6bf457
Sara Schoonover <Saramschoon@gmail.com>
2023-11-13 16:57:05 +0000

    added liked photos path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(username: params.fetch(:username))
+  end
 
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
   end
 
 end
